fc98edb63fbb998984db4c40aaba3d2eb64b65a9
rashedabir <rashed15-2155@diu.edu.bd>
2021-08-27 13:38:54 +0600

    list, mapping

---
+++ list.dart
@@ -0,0 +1,8 @@
+void main() {
+  List lst = [];
+  lst.add(5);
+  lst.add(10);
+  lst.add("rashed");
+  print(lst);
+  lst.clear();
+}
---
+++ mapping.dart
@@ -0,0 +1,10 @@
+void main() {
+  // var details = {'Username': 'rashed', 'Password': 'pass@2155'};
+  // details['Id'] = '182-15-2155';
+  // print(details);
+  var details = new Map();
+  details['Id'] = "182-15-2155";
+  details['Username'] = "rashed";
+  details['Password'] = "pass@2155";
+  print(details);
+}
